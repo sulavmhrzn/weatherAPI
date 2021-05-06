a3d285d7186837286c8452ca6cd3db2f5ffc897e
Sulav <sulavmaharjan63@gmail.com>
2021-05-06 17:47:48 +0545

    Initial setup

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import json
+import fastapi
+import uvicorn
+from pathlib import Path
+from views import home
+
+api = fastapi.FastAPI()
+api.include_router(home.router)
+
+def configure():
+    configure_json()
+
+
+def configure_json():
+    json_path = Path("settings.json").absolute()
+    if not os.path.exists(json_path):
+        print("settings.json file was not found. The script will now terminate.")
+        raise FileNotFoundError()
+    with open("settings.json") as f:
+        settings = json.load(f)
+
+
+if __name__ == "__main__":
+    configure()
+    uvicorn.run(api)
+else:
+    configure()
