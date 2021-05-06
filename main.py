420d549f385e73b5e3fb3edc363e451e0776786a
Sulav <sulavmaharjan63@gmail.com>
2021-05-06 17:53:40 +0545

    Setup templates, staticfiles

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import json
 import fastapi
 import uvicorn
 from pathlib import Path
+from starlette.staticfiles import StaticFiles
 from views import home
 
 api = fastapi.FastAPI()
 api.include_router(home.router)
+api.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 def configure():
     configure_json()
--- views/home.py
+++ views/home.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from starlette.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates("templates")
 
 
 @router.get("/")
-def home():
-    return "Home"
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
